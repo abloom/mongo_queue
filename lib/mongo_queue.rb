cc543c48ec7a5719ae58258c225cdceba8df2878
Andrew Bloom <andrew.bloom@centro.net>
2010-06-21 13:43:08 -0500

    Namespacing issues with OrderedHash

--- lib/mongo_queue.rb
+++ lib/mongo_queue.rb
@@ -48,7 +48,7 @@ class Mongo::Queue
   # Example:
   #    locked_doc = queue.lock_next(Thread.current.object_id)
   def lock_next(locked_by)
-    cmd = OrderedHash.new
+    cmd = BSON::OrderedHash.new
     cmd['findandmodify'] = @config[:collection]
     cmd['update']        = {'$set' => {:locked_by => locked_by, :locked_at => Time.now.utc}}
     cmd['query']         = {:locked_by => nil, :locked_by => nil, :attempts => {'$lt' => @config[:attempts]}}
@@ -70,7 +70,7 @@ class Mongo::Queue
 
   # Release a lock on the specified document and allow it to become available again.
   def release(doc, locked_by)
-    cmd = OrderedHash.new
+    cmd = BSON::OrderedHash.new
     cmd['findandmodify'] = @config[:collection]
     cmd['update']        = {'$set' => {:locked_by => nil, :locked_at => nil}}
     cmd['query']         = {:locked_by => locked_by, :_id => BSON::ObjectID.from_string(doc['_id'].to_s)}
@@ -82,7 +82,7 @@ class Mongo::Queue
   # Remove the document from the queue. This should be called when the work is done and the document is no longer needed.
   # You must provide the process identifier that the document was locked with to complete it.
   def complete(doc, locked_by)
-    cmd = OrderedHash.new
+    cmd = BSON::OrderedHash.new
     cmd['findandmodify'] = @config[:collection]
     cmd['query']         = {:locked_by => locked_by, :_id => BSON::ObjectID.from_string(doc['_id'].to_s)}
     cmd['remove']        = true
@@ -127,7 +127,7 @@ class Mongo::Queue
   protected
 
   def sort_hash #:nodoc:
-    sh = OrderedHash.new
+    sh = BSON::OrderedHash.new
     sh['priority'] = -1 ; sh
   end
 
